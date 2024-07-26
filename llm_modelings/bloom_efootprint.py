b1f01f902aa4af6c4768eeeb0137dc0e0438d17b
Vincent Villet <vincent.villet@gmail.com>
2024-07-26 10:32:26 +0200

    [UPDATE] Bloom modeling to efootprint 2.1.6

--- llm_modelings/bloom_efootprint.py
+++ llm_modelings/bloom_efootprint.py
@@ -1,5 +1,6 @@
 from efootprint.abstract_modeling_classes.source_objects import SourceValue, Sources, SourceObject, Source
 from efootprint.core.usage.user_journey import UserJourney, UserJourneyStep
+from efootprint.core.usage.job import Job
 from efootprint.core.hardware.servers.autoscaling import Autoscaling
 from efootprint.core.hardware.storage import Storage
 from efootprint.core.service import Service
@@ -10,32 +11,30 @@ from efootprint.core.hardware.network import Network
 from efootprint.core.system import System
 from efootprint.constants.countries import Countries
 from efootprint.constants.units import u
-from efootprint.utils.object_relationships_graphs import build_object_relationships_graph, \
-    USAGE_PATTERN_VIEW_CLASSES_TO_IGNORE
-from efootprint.utils.calculus_graph import build_calculus_graph
 
-import os
 
 bloom_paper_source = Source("Estimating the Carbon Footprint of BLOOM", "https://arxiv.org/abs/2211.05100")
 
 gpu_hours = SourceValue(1082000 * u.h, bloom_paper_source, "gpu_hours")
 gpu_lifetime = SourceValue(6 * u.year, bloom_paper_source, "gpu_lifetime")
-total_training_time = SourceValue(119 * u.day, bloom_paper_source,"total_training_time")
+total_training_time = SourceValue(119 * u.day, bloom_paper_source, "total_training_time")
 gpu_power = SourceValue(400 * u.W, bloom_paper_source, "GPU Power")
 gpu_idle_power = SourceValue(50 * u.W, bloom_paper_source, "GPU idle power")
 
-
 NB_OF_GPUs = (gpu_hours / total_training_time).set_label("Number of Gpus")
 
-carbon_footprint_fabrication_server_without_gpu = SourceValue(2500 * u.kg, bloom_paper_source, "Carbon footprint without GPU")
+carbon_footprint_fabrication_server_without_gpu = SourceValue(
+    2500 * u.kg, bloom_paper_source, "Carbon footprint without GPU")
 carbon_footprint_fabrication_one_gpu = SourceValue(150 * u.kg, bloom_paper_source, "Carbon footprint one GPU")
 
-cpu_core_per_gpu = SourceValue(1 * u.core, label= "1 CPU core / GPU")
-cpu_core_per_gpu_uj = SourceValue(1 * u.core / u.uj, label= "1 CPU core / GPU / uj")
+cpu_core_per_gpu = SourceValue(1 * u.core, label="1 CPU core / GPU")
+cpu_core_per_gpu_uj = SourceValue(1 * u.core / u.uj, label="1 CPU core / GPU / uj")
 
 server = Autoscaling(
     "Training GPU server",
-    carbon_footprint_fabrication=(carbon_footprint_fabrication_server_without_gpu + NB_OF_GPUs * carbon_footprint_fabrication_one_gpu).set_label("default"),
+    carbon_footprint_fabrication=(
+        carbon_footprint_fabrication_server_without_gpu + NB_OF_GPUs * carbon_footprint_fabrication_one_gpu
+        ).set_label("default"),
     power=(gpu_power * NB_OF_GPUs).set_label("default"),
     lifespan=SourceValue(6 * 0.85 * u.year, bloom_paper_source),
     idle_power=(gpu_idle_power * NB_OF_GPUs).set_label("default"),
@@ -63,11 +62,12 @@ service = Service(
     base_cpu_consumption=SourceValue(0 * u.core, Sources.HYPOTHESIS))
 
 bloom_training_step = UserJourneyStep(
-    "Bloom training", service, SourceValue(1 * u.TB / u.uj, Sources.USER_DATA),
-    SourceValue(1 * u.TB / u.uj, Sources.USER_DATA),
-    user_time_spent=SourceValue(24 * u.h / u.uj, Sources.USER_DATA),
-    request_duration=SourceValue(24 * u.h, Sources.HYPOTHESIS),
-    cpu_needed=(NB_OF_GPUs * cpu_core_per_gpu_uj).set_label("default"))
+    "Bloom training", user_time_spent=SourceValue(24 * u.h / u.uj, Sources.USER_DATA),
+    jobs=[Job("Bloom training job", service, SourceValue(1 * u.TB / u.uj, Sources.USER_DATA),
+              SourceValue(1 * u.TB / u.uj, Sources.USER_DATA),
+              request_duration=SourceValue(24 * u.h, Sources.HYPOTHESIS),
+              cpu_needed=(NB_OF_GPUs * cpu_core_per_gpu_uj).set_label("default"),
+              ram_needed=SourceValue(100 * u.MB / u.uj, Sources.HYPOTHESIS))])
 
 training_uj = UserJourney("Training", uj_steps=[bloom_training_step])
 
@@ -85,7 +85,9 @@ GPU_INFERENCE_NB = SourceValue(16 * u.dimensionless)
 
 server_inference = Autoscaling(
     "Inference GPU server",
-    carbon_footprint_fabrication=(carbon_footprint_fabrication_server_without_gpu + GPU_INFERENCE_NB * carbon_footprint_fabrication_one_gpu).set_label("default"),
+    carbon_footprint_fabrication=(
+        carbon_footprint_fabrication_server_without_gpu + GPU_INFERENCE_NB * carbon_footprint_fabrication_one_gpu
+        ).set_label("default"),
     power=(GPU_INFERENCE_NB * gpu_power).set_label("default"),
     lifespan=SourceValue(6 * 0.85 * u.year, bloom_paper_source),
     idle_power=(GPU_INFERENCE_NB * gpu_idle_power).set_label("default"),
@@ -106,11 +108,14 @@ average_utilisation_rate = average_cluster_power_measured / server_inference.pow
 request_duration = average_utilisation_rate / request_per_hour
 
 bloom_inference_step = UserJourneyStep(
-    "Bloom inference", service_inference, SourceValue(100 * u.kB / u.uj, Sources.USER_DATA),
-    SourceValue(100 * u.kB / u.uj, Sources.USER_DATA),
-    user_time_spent=request_duration * SourceValue(1 / u.user_journey, label="One request per user session"),
-    request_duration=request_duration,
-    cpu_needed=NB_OF_GPUs * cpu_core_per_gpu_uj)
+    "Bloom inference",
+    user_time_spent=(
+        request_duration * SourceValue(1 / u.user_journey, label="One request per user session")).set_label("default"),
+    jobs=[Job("Bloom inference job", service_inference, SourceValue(100 * u.kB / u.uj, Sources.USER_DATA),
+              SourceValue(100 * u.kB / u.uj, Sources.USER_DATA),
+              request_duration=request_duration.set_label("default"),
+              cpu_needed=(NB_OF_GPUs * cpu_core_per_gpu_uj).set_label("default"),
+              ram_needed=SourceValue(100 * u.MB / u.uj, Sources.HYPOTHESIS))])
 
 inference_uj = UserJourney("Inference", uj_steps=[bloom_inference_step])
 
@@ -126,12 +131,5 @@ system = System("Bloom usage in France", [inference_up, training_up])
 print(f"Server carbon footprint is {(server.energy_footprint + server.instances_fabrication_footprint).value}")
 print(f"Total system carbon footprint is {system.total_footprint.value}")
 
-object_relationships_graph = build_object_relationships_graph(
-    system, classes_to_ignore=USAGE_PATTERN_VIEW_CLASSES_TO_IGNORE)
-object_relationships_graph.show(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "object_relationships_graph.html"))
-
-graph = build_calculus_graph(server.instances_fabrication_footprint)
-graph.show(os.path.join(os.path.abspath(os.path.dirname(__file__)), "server_fab_calculation_graph.html"))
-
 system.plot_footprints_by_category_and_object()
+system.object_relationship_graph_to_file("Bloom object relationship graph.html")
